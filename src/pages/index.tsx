7b20b30383b4d9be2cdd82294598fa64e78180a3
SauravKoshti <sauravkoshti2000@gmail.com>
2024-12-19 16:50:59 +0530

    css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image"
 
-import Logo from "../assets/images/b_day.JPG";
+import Logo from "../assets/images/pic.jpg";
 
 export default function Home() {
   return (
@@ -17,10 +17,10 @@ export default function Home() {
     <div className="cardInside">
         <h3 className="back">HAPPY BIRTHDAY Meghna Di!</h3>
   <div className="mx-auto">
-  <Image src={Logo} height={80} width={80} alt="Birthday" />
+  <Image src={Logo} height={130} width={150} alt="Birthday" />
   </div>
         <p>Dear Friend,</p> 
-        <p> 
+        <p>  
             Happy birthday!! I hope your day is filled with lots of love and
             laughter! May all of your birthday wishes come true.
         </p>
